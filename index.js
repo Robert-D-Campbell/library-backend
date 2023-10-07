413da77f525ff6a6d4a1c69d1e314530b12703d0
Robert Campbell <robert.campbell1191@gmail.com>
2023-10-07 11:52:50 -0700

    feat: set up database connection

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const mongoConnectStr = process.env.DB_URL;
+
+mongoose.connect(mongoConnectStr);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Connected to MongoDB");
+});
 
 const app = express();
 
